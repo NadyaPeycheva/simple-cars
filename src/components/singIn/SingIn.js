6c3c45665714426fa65cb34d76c0cf045e563415
Nadya Peycheva <peicheva98@abv.bg>
2022-12-21 19:00:27 +0200

    commit login page

--- src/components/singIn/SingIn.js
+++ src/components/singIn/SingIn.js
@@ -11,22 +11,41 @@ const SingIn = () => {
 
   const submitHandler = (event) => {
     event.preventDefault();
+
+    const username=userNameInput.current.value;
+    const password=passwordInput.current.value;
+
+    if(!username||!password) {
+        return;
+    }
+
+    fetch('http://161.35.202.170:8080/users/login',{
+        method: 'POST',
+        body:JSON.stringify({username,password}),
+        headers: {'Content-Type': 'application/json'}
+    }).then((response) => {
+        response.json().then((data) => {
+            console.log(data);
+            userNameInput='';
+            passwordInput='';
+        })
+    })
   };
 
   return (
     <Background>
       <div className={classes.formSingInContainer}>
-        <h4 className={classes.formSingTitle}>Sing ip</h4>
+        <h4 className={classes.formSingTitle}>Sing in</h4>
 
         <form onSubmit={submitHandler}>
           <fieldset>
             <legend>User name *</legend>
-            <input ref={userNameInput} placeholder='password' />
+            <input ref={userNameInput}  />
           </fieldset>
 
           <input type="text" placeholder="Password *" ref={passwordInput} />
 
-          <button type="submit">SING UP</button>
+          <button type="submit">SING IN</button>
         </form>
         <Link to="catalog" className={classes.link}>
           <p>Don't have an account ?</p> <p>Continue to catalog</p>
